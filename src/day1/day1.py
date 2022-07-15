1bf4c105ff9572f3e3fc47e6e005a5486d312377
trevorgarr <garrood.trevor@gmail.com>
2022-07-14 19:31:04 -0700

    First commit

---
+++ src/day1/day1.py
@@ -0,0 +1,19 @@
+"""
+Advent of Code Day 1:
+Submarine Depths
+Trevor Garrood
+"""
+
+
+def part_one(nums):
+
+
+
+def part_two(nums):
+
+
+
+if __name__ == "__main__":
+    f = open("day1.txt", "r")
+    data = f.read()
+    nums = data.splitlines()
